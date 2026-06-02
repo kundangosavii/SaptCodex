08ef24ed66463610383dc62d67f4de04b245e6f7
Kundan Gosavi <gosavikundan4@gmail.com>
2026-06-02 20:30:04 +0530

    add JWT verification to getTasks route

--- Backend/src/modules/task/task.routes.js
+++ Backend/src/modules/task/task.routes.js
@@ -1,8 +1,9 @@
 import { Router } from 'express'; 
 import { getTasksController } from './task.controller.js';
+import { verifyJWT } from '../../middleware/auth.middleware.js';
 
 const router = Router();
 
-router.post('/gettasks', getTasksController);
+router.post('/gettasks', verifyJWT, getTasksController);
 
 export default router;
