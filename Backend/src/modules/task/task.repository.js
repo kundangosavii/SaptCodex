e1f9a9b5955b8e2be5368bc499c24847843686af
Kundan Gosavi <gosavikundan4@gmail.com>
2026-06-02 20:29:19 +0530

    change getLevelOfUser function to use async/await

--- Backend/src/modules/task/task.repository.js
+++ Backend/src/modules/task/task.repository.js
@@ -1,12 +1,15 @@
-const getLevelOfUser = (userId) => {
-    const user= User.findById(userId).select('onboarding.level').lean();
-    if(!user) {
+import User from '../user/user.model.js';
+
+const getLevelOfUser = async (userId) => {
+    const user = await User.findById(userId).select('onboarding.level').lean();
+
+    if (!user) {
         throw new Error('User not found');
     }
 
-    return user;
+    return user.onboarding?.level;
 }
 
 export {
-    getLevelOfUser  
+    getLevelOfUser
 }
